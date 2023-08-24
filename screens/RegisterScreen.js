62bb321cdfb7567f9525d2ca18cc80b605d07c8c
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-24 19:43:22 +0300

    edited

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,5 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Pressable,
+  TextInput,
+  KeyboardAvoidingView,
+  Image,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const RegisterScreen = () => {
   return (
