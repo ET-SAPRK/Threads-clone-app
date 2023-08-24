913dc2c63099da3f898d58998fe5dec6e4775ccb
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-24 23:00:01 +0300

    edited

--- api/index.js
+++ api/index.js
@@ -27,3 +27,63 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.listen(port, () => {
 console.log("server is running on port 3000");
 });
+
+const User = require("./models/user");
+const Post = require("./models/post");
+
+app.post("//register", async(req,res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+    const newUser = new User({ name, email, password });
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+    await newUser.save();
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+    res.status(200).json({ message: "Registration successful"});
+  } catch (error) {
+    console.log("error registering user", error);
+    res.status(500).json({ message: "error registering user" });
+  }  
+})
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.APP_USER,
+      pass: process.env.APP_PASS,
+    },
+  });
+  const mailOptions = {
+    from: "threads.com",
+    to: email,
+    subject: "Email Verification",
+    text: `please click the following link to verify your email http://localhost:3000/verify/${verificationToken}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("error sending email", error);
+  }
+}
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid token" });
+    }
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    console.log("error getting token", error);
+    res.status(500).json({ message: "Email verification failed" });
+  }
+})
