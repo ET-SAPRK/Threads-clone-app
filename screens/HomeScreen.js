517bd185d828c11edc9473a9e302855b598294ca
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-25 10:33:33 +0300

    edited

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,7 +12,6 @@ import { useFocusEffect } from "@react-navigation/native";
 const HomeScreen = () => {
   const { userId, setUserId } = useContext(UserType);
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
     const fetchUsers = async () => {
       const token = await AsyncStorage.getItem("authToken");
@@ -23,10 +22,15 @@ const HomeScreen = () => {
 
     fetchUsers();
   }, []);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-  useEffect(() =>{
-    fetchPosts()
-  },[])
+  useFocusEffect(
+    useCallback(() => {
+      fetchPosts();
+    }, [])
+  );
 
   const fetchPosts = async () => {
     try {
@@ -36,15 +40,127 @@ const HomeScreen = () => {
       console.log("error fetching posts", error);
     }
   };
+
   console.log("posts", posts);
+  const handleLike = async (postId) => {
+    try {
+      const response = await axios.put(
+        `http://192.168.43.207:3000/posts/${postId}/${userId}/like`
+      );
+      const updatedPost = response.data;
 
+      const updatedPosts = posts?.map((post) =>
+        post?._id === updatedPost._id ? updatedPost : post
+      );
+
+      setPosts(updatedPosts);
+    } catch (error) {
+      console.log("Error liking the post", error);
+    }
+  };
+
+  const handleDislike = async (postId) => {
+    try {
+      const response = await axios.put(
+        `http://192.168.43.207:3000/posts/${postId}/${userId}/unlike`
+      );
+      const updatedPost = response.data;
+      // Update the posts array with the updated post
+      const updatedPosts = posts.map((post) =>
+        post._id === updatedPost._id ? updatedPost : post
+      );
+      console.log("updated ",updatedPosts)
+    
+      setPosts(updatedPosts);
+    } catch (error) {
+      console.error("Error unliking post:", error);
+    }
+  };
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ marginTop: 50, flex: 1, backgroundColor: "white" }}>
+      <View style={{ alignItems: "center", marginTop: 20 }}>
+        <Image
+          style={{ width: 60, height: 40, resizeMode: "contain" }}
+          source={{
+            uri: "https://freelogopng.com/images/all_img/1688663386threads-logo-transparent.png",
+          }}
+        />
+      </View>
 
-export default HomeScreen
+      <View style={{ marginTop: 20 }}>
+        {posts?.map((post) => (
+          <View
+            style={{
+              padding: 15,
+              borderColor: "#D0D0D0",
+              borderTopWidth: 1,
+              flexDirection: "row",
+              gap: 10,
+              marginVertical: 10,
+            }}
+          >
+            <View>
+              <Image
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  resizeMode: "contain",
+                }}
+                source={{
+                  uri: "https://cdn-icons-png.flaticon.com/128/149/149071.png",
+                }}
+              />
+            </View>
 
-const styles = StyleSheet.create({})
+            <View>
+              <Text
+                style={{ fontSize: 15, fontWeight: "bold", marginBottom: 4 }}
+              >
+                {post?.user?.name}
+              </Text>
+              <Text>{post?.content}</Text>
+
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 10,
+                  marginTop: 15,
+                }}
+              >
+                {post?.likes?.includes(userId) ? (
+                  <AntDesign
+                    onPress={() => handleDislike(post?._id)}
+                    name="heart"
+                    size={18}
+                    color="red"
+                  />
+                ) : (
+                  <AntDesign
+                    onPress={() => handleLike(post?._id)}
+                    name="hearto"
+                    size={18}
+                    color="black"
+                  />
+                )}
+
+                <FontAwesome name="comment-o" size={18} color="black" />
+
+                <Ionicons name="share-social-outline" size={18} color="black" />
+              </View>
+
+              <Text style={{ marginTop: 7, color: "gray" }}>
+                {post?.likes?.length} likes • {post?.replies?.length} reply
+              </Text>
+            </View>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
