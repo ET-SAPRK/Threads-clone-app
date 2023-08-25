8351c23983cd3bf0695390d965b397953f9d1d0c
ET-SPARK <86984205+ET-SAPRK@users.noreply.github.com>
2023-08-25 09:41:33 +0300

    edited

--- api/index.js
+++ api/index.js
@@ -7,17 +7,19 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 const port = 3000;
-const cors = require('cors')
+const cors = require("cors");
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const jwt = require("jsonwebtoken");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+  })
+  .then(() => {
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
@@ -25,13 +27,13 @@ mongoose.connect(process.env.MONGODB_URI, {
   });
 
 app.listen(port, () => {
-console.log("server is running on port 3000");
+  console.log("server is running on port 3000");
 });
 
 const User = require("./models/user");
 const Post = require("./models/post");
 
-app.post("/register", async(req,res) => {
+app.post("/register", async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
@@ -43,12 +45,12 @@ app.post("/register", async(req,res) => {
     newUser.verificationToken = crypto.randomBytes(20).toString("hex");
     await newUser.save();
     sendVerificationEmail(newUser.email, newUser.verificationToken);
-    res.status(200).json({ message: "Registration successful"});
+    res.status(200).json({ message: "Registration successful" });
   } catch (error) {
     console.log("error registering user", error);
     res.status(500).json({ message: "error registering user" });
-  }  
-})
+  }
+});
 
 const sendVerificationEmail = async (email, verificationToken) => {
   const transporter = nodemailer.createTransport({
@@ -69,7 +71,7 @@ const sendVerificationEmail = async (email, verificationToken) => {
   } catch (error) {
     console.log("error sending email", error);
   }
-}
+};
 
 app.get("/verify/:token", async (req, res) => {
   try {
@@ -86,7 +88,7 @@ app.get("/verify/:token", async (req, res) => {
     console.log("error getting token", error);
     res.status(500).json({ message: "Email verification failed" });
   }
-})
+});
 
 const generateSecretKey = () => {
   const secretKey = crypto.randomBytes(32).toString("hex");
@@ -111,7 +113,7 @@ app.post("/login", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Login failed" });
   }
-})
+});
 
 app.get("/user/:userId", (req, res) => {
   try {
@@ -156,5 +158,81 @@ app.post("/users/unfollow", async (req, res) => {
     res.status(200).json({ message: "Unfollowed successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error unfollowing user" });
+  }
+});
+
+app.post("/create-post", async (req, res) => {
+  try {
+    const { content, userId } = req.body;
+
+    const newPostData = {
+      user: userId,
+    };
+
+    if (content) {
+      newPostData.content = content;
+    }
+
+    const newPost = new Post(newPostData);
+    await newPost.save();
+
+    res.status(200).json({ message: "Post saved successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "post creation failed" });
+  }
+});
+
+app.put("/posts/:postId/:userId/like", async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+
+  try {
+    const post = await Post.findById(postId).populate("user", "name");
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $addToSet: { likes: userId } }, 
+      { new: true } 
+    );
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    updatedPost.user = post.user;
+
+    res.json(updatedPost);
+  } catch (error) {
+    console.error("Error liking post:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while liking the post" });
+  }
+});
+
+app.put("/posts/:postId/:userId/unlike", async (req, res) => {
+  const postId = req.params.postId;
+  const userId = req.params.userId;
+
+  try {
+    const post = await Post.findById(postId).populate("user", "name");
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { $pull: { likes: userId } },
+      { new: true }
+    );
+
+    updatedPost.user = post.user;
+
+    if (!updatedPost) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    res.json(updatedPost);
+  } catch (error) {
+    console.error("Error unliking post:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred while unliking the post" });
   }
 });
